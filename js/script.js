635e4f303503fd376c1f5046310c6e687ede64a8
Olivia Ten Den <olivia.ten-den@stu.ocsb.ca>
2025-05-07 01:38:47 +0000

    added code

--- js/script.js
+++ js/script.js
@@ -6,14 +6,46 @@
 
 "use strict"
 
-function checkDiscount() {
-  let age = parseInt(document.getElementById("age").value);
-  let dayOfTheWeek = document.getElementById("day");
-  let value = dayOfTheWeek.options[dayOfTheWeek.selectedIndex].value;
+function calculateCost() {
+  let pastry = document.getElementById("pastry")
+  let value = pastry.options[pastry.selectedIndex].value
+  let quantity = parseInt(document.getElementById("quantity").value)
+  let itemCost = 0
+  let discountRate = 0
+  let discount = 0
+  let subTotal = 0
+  let taxRate = 0.13
+  let tax = 0
+  let total = 0
 
-  if (value == "tuesday" || value == "thursday" || (age > 12 && age < 21)) {
-    document.getElementById('result').innerHTML = "<p>You are eligible for the discount.</p>";
+  if (value == "cupcake") {
+    itemCost = 2.00
+  } else if (value == "donut") {
+    itemCost = 1.50
+  } else if (value == "mini-cheesecake") {
+    itemCost = 3.00
+  } else if (value == "macaron") {
+    itemCost = 1.00
   } else {
-    document.getElementById('result').innerHTML = "<p>You are NOT eligible for the discount.</p>";
+    itemCost = 2.50
   }
+
+  if (quantity > 5) {
+    discountRate = 0.10
+  }
+
+  subTotal = quantity * itemCost
+  discount = subTotal * discountRate
+  tax = (subTotal - discount) * taxRate
+  total = subTotal - discount + tax
+  subTotal = subTotal.toFixed(2)
+  discount = discount.toFixed(2)
+  tax = tax.toFixed(2)
+  total = total.toFixed(2)
+
+  document.getElementById('items').innerHTML = "<p>" + quantity + " " + value + "(s)</p>"
+  document.getElementById('subTotal').innerHTML = "<p>Subtotal: $" + subTotal + "</p>"
+  document.getElementById('discount').innerHTML = "<p>Discount: $" + discount + "</p>"
+  document.getElementById('tax').innerHTML = "<p>Tax: $" + tax + "</p>"
+  document.getElementById('total').innerHTML = "<p>Total: $" + total + "</p>"
 }
